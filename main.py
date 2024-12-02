a92f55eb97579d67ee9b3d1f3372ed2075cebeb5
DjDango <168477920+DjDango@users.noreply.github.com>
2024-12-02 12:51:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
+count = 0
+
+def click():
+  count +=1
 
 root = Tk()
 root.size(500x500)
 root.title('ClickerTest')
+
+Lbl = Label(root, font=('Monospace'), 50, fg='black', bg='white', text=count)
+Lbl.pack()
 
 root.mainloop()
 
